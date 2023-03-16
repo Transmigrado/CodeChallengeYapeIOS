6e58da5a9bff2d61925b5435b3e08f885fad5d27
Jorge Acosta <transmigrado@gmail.com>
2023-03-16 14:52:11 -0300

    feat: first commit

---
+++ YapeRecipes/YapeRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  YapeRecipesApp.swift
+//  YapeRecipes
+//
+//  Created by Jorge Acosta Alvarado on 16-03-23.
+//
+
+import SwiftUI
+
+@main
+struct YapeRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
