6cd0810a04e390d41d0a8e5652d8016d5138e9fe
Jorge Acosta <transmigrado@gmail.com>
2023-03-18 18:31:10 -0300

    feat: add AppState

---
+++ YapeRecipes/Redux/Core/AppState.swift
@@ -0,0 +1,28 @@
+//
+//  AppState.swift
+//  YapeRecipes
+//
+//  Created by Jorge Acosta Alvarado on 18-03-23.
+//
+
+
+import Foundation
+import ReSwift
+import SwiftUI
+
+struct AppState: Equatable {
+
+    var stateStatus = UUID()
+    var recipes = Recipes()
+}
+
+//MARK: RECIPES
+struct Recipes: Identifiable, Equatable {
+    static func == (lhs: Recipes, rhs: Recipes) -> Bool {
+        return lhs.id == rhs.id
+    }
+    
+    var id = UUID()
+    var list: [Recipe] = []
+}
+
